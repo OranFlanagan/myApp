662158e3e0de8d1d3b5fbe4314679f7d0bb87ed7
OranFlanagan <163148816+OranFlanagan@users.noreply.github.com>
2025-04-25 15:03:02 +0100

    comments added

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,30 +1,53 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { ReactiveFormsModule, FormBuilder, Validators, } from '@angular/forms';
-import { RouterLink} from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.page.html',
+  templateUrl: './login.page.html', 
   styleUrls: ['./login.page.scss'],
-  standalone: true,
+  standalone: true,              
   imports: [IonicModule, ReactiveFormsModule, RouterLink],
 })
-export class LoginPage 
-{
-  loginForm= this.fb.group
-  (
-    {
-    name:    ['', [Validators.required, Validators.minLength(2)]],
-    email:   ['', [Validators.required, Validators.email]],
-    password:['', [Validators.required, Validators.minLength(6)]]
-    }
-  );
+export class LoginPage {
+  // Define the reactive form using FormBuilder
+  loginForm = this.fb.group({
+    name: ['',                        // Initial value for the 'name' control
+      [
+        Validators.required,     // Name is required
+        Validators.minLength(2)  // Minimum length of 2 characters
+      ]
+    ],
+    email: [
+      '',                        // Initial value for the 'email' control
+      [
+        Validators.required,     // Email is required
+        Validators.email         // Must be a valid email format
+      ]
+    ],
+    password: [
+      '',                        // Initial value for the 'password' control
+      [
+        Validators.required,     // Password is required
+        Validators.minLength(6)  // Minimum length of 6 characters
+      ]
+    ]
+  });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder        // Inject FormBuilder to create form group
+  ) {}
 
   async onSubmit() {
+    if (this.loginForm.invalid) {
+      // If the form is invalid, you might show errors or return early
+      return;
+    }
+    
     const { name } = this.loginForm.value;
+
+    console.log('Logging in user:', name);
   }
 }
